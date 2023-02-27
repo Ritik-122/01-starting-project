da91be0003640455f04f5b8748c08ea1a0eed813
ritik sharma <bharadwajritik1@gmail.com>
2023-02-28 03:11:41 +0530

    "Loading added "

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const[movies,setMovies]=useState([])
-  const fetchMovieHandler=async()=>{
-    const d= (await fetch('https://swapi.py4e.com/api/films/'))
-    const res=await d.json()
-     setMovies(res.results)
-    
-  }
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const fetchMovieHandler = async () => {
+    setLoading(true)
+    const d = await fetch("https://swapi.py4e.com/api/films/");
+    const res = await d.json();
+    setMovies(res.results);
+    setLoading(false)
+  };
 
   return (
     <React.Fragment>
@@ -18,7 +20,8 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading....</p>}
       </section>
     </React.Fragment>
   );
