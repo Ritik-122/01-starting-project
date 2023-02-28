7ac5fb7ead7b04039276db2b655fd97a636aa2f4
ritik sharma <bharadwajritik1@gmail.com>
2023-02-28 15:28:10 +0530

    "retry added"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,11 +8,11 @@ function App() {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMovieHandler = useCallback( async () => {
+  const fetchMovieHandler = useCallback(async () => {
     setLoading(true);
     setError(null);
     try {
-      const d = await fetch("https://swapi.py4e.com/api/films/");
+      const d = await fetch("https://swapi.py4e.com/api/fims/");
 
       if (!d.ok) {
         throw new Error("...Retrying");
@@ -23,26 +23,44 @@ function App() {
       setError(error.message);
     }
     setLoading(false);
-  },[]);
-  
-  useEffect(()=>{
-    fetchMovieHandler()
-    },[fetchMovieHandler])
+  }, []);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
 
   let content = <p>Found no movies</p>;
+
+  let tt;
+  const stopRetrying=()=>{
+    clearInterval(tt)
+    setError(false)
+   
+  }
+   if(error){content = (
+    <>
+      <p>
+        {error}
+        <button onClick={stopRetrying}>Stop</button>
+      </p>
+    </>
+  );
+   }
+ 
+  useEffect(()=>{
+    if (error) {
+      
+       tt = setInterval(fetchMovieHandler, 5000);
+      
+    }
+   return (()=>clearInterval(tt))
+  })
+    
+ 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
-  if (error) {
-    content = (
-      <>
-        <p>
-          {error}
-          
-        </p>
-      </>
-    );
-  }
+
   if (isLoading) {
     content = <p>Loading...</p>;
   }
