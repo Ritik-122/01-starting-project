3865b92011d7ae8701e9eea42a28501d91f90be4
ritik sharma <bharadwajritik1@gmail.com>
2023-02-28 15:02:07 +0530

    "bug free and on Load api call"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState,useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,23 +6,53 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setLoading] = useState(false);
-  const fetchMovieHandler = async () => {
-    setLoading(true)
-    const d = await fetch("https://swapi.py4e.com/api/films/");
-    const res = await d.json();
-    setMovies(res.results);
-    setLoading(false)
-  };
+  const [error, setError] = useState(null);
+
+  const fetchMovieHandler = useCallback( async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const d = await fetch("https://swapi.py4e.com/api/films/");
+
+      if (!d.ok) {
+        throw new Error("...Retrying");
+      }
+      const res = await d.json();
+      setMovies(res.results);
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
+  },[]);
+  
+  useEffect(()=>{
+    fetchMovieHandler()
+    },[fetchMovieHandler])
+
+  let content = <p>Found no movies</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = (
+      <>
+        <p>
+          {error}
+          
+        </p>
+      </>
+    );
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && <MoviesList movies={movies} />}
-        {isLoading && <p>Loading....</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
